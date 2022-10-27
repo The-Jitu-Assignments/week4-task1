cb75cd9992d1a78e1a53ca135e3b03abd1dae2b4
john katua <johnkatua99@gmail.com>
2022-10-27 05:38:09 +0300

    worked on the list header

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const TodoList = () => {
   return (
-    <div>TodoList</div>
+    <div className='list--box'>
+      <div className='todo--header'>
+        <div className='todo--searchbar'>
+          <input />
+        </div>
+        <div className='list--priority__box'>
+          <div className="list--priority">item1</div>
+          <div className="list--priority">item2</div>
+          <div className="list--priority">item3</div>
+          <div className="list--priority">item4</div>
+        </div>
+      </div>
+      <div className='todo--body'>Todo Body</div>
+    </div>
   )
 }
 
